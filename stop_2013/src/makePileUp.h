53d86860b42605aabe82da6142c8b89f5fe8498f
Francesco Costanza <fcost@nafhh-cms02.desy.de>
2014-02-24 17:21:49 +0100

    Getting Readay for the Bonsai

--- stop_2013/src/makePileUp.h
+++ stop_2013/src/makePileUp.h
@@ -2,13 +2,9 @@
 #define makePileUp_h
 
 #include "ConfigReader.h"
-#include "SampleInformation.h"
-#include "NtupleTools2.h"
+#include "TString.h"
 
-ConfigReader config;
 class mySampleInfo; //forward declaration
-
-
 
 class pileUpInfo{
 private:
@@ -22,6 +18,7 @@ private:
   double PUWeight_up;
   double PUWeight_down;
   int relevantNumPU;
+  //
 
 
 public:
@@ -30,9 +27,9 @@ public:
   vector<double> PUdata_up;
   vector<double> PUdata_down;
 
-  pileUpInfo(SampleInformation& mySampleInfo){
+  pileUpInfo(const TString& WhatSample, const TString& WhatSubSample){
     this->Constructor();
-    this->Initialize(mySampleInfo);
+    this->Initialize( WhatSample, WhatSubSample);
   }
   pileUpInfo(){
     this->Constructor();
@@ -49,13 +46,11 @@ public:
     PUWeight_down=1;
     relevantNumPU=0;
   }
-  
-  int Initialize(SampleInformation& mySampleInfo){
+
+  int Initialize( const TString& WhatSample, const TString& WhatSubSample){
     //PU
-  
+    
     ConfigReader config;
-    string WhatSample=mySampleInfo.GetSample();
-    string WhatSubSample=mySampleInfo.GetSubSample();
 
     if(!oldpuw){
       nobinsmc = config.getDouble("PU_"+WhatSample+"_"+WhatSubSample+"_mc",PUmc," ");
@@ -66,64 +61,17 @@ public:
     else if(oldpuw){
       nobinsmc = config.getDouble("oldPU_"+WhatSample+"_"+WhatSubSample+"_mc",PUmc," ");
     } 
-
+    
     if(nobinsmc!=nobinsdata && !oldpuw){
       cout << "problem in pu inf in para_config - number of bins in MC PU dist is different than data!" << endl;
       return -1;
-    }
-  
+    }  
     return 0;
   }
-
-
-
-  void RescaleWeight(EasyChain* tree, double& weight, string type=""){
   
-    float PUnumInter    = tree->Get( PUnumInter, "pileupTrueNumInteractionsBX0");
-    relevantNumPU = (int) PUnumInter;
-    if( relevantNumPU >= nobinsmc ) {
-      cout << "something wrong with the pile up info!!! - exceed max number of vertex:     " << nobinsmc <<endl;
-      return; 
-    }
   
-    else if( oldpuw) {
-      PUWeight= PUmc.at( relevantNumPU );
-    }
-    else {
-      if (type=="" || type=="central"){
-	PUWeight= PUdata.at( relevantNumPU )/PUmc.at( relevantNumPU );
-      }else if (type=="up"){
-	PUWeight_up= PUdata_up.at( relevantNumPU )/PUmc.at( relevantNumPU );
-      }else if (type=="down"){
-	PUWeight_down= PUdata_down.at( relevantNumPU )/PUmc.at( relevantNumPU );
-      }else{
-	cout<<"wrong pileup weight chice. ERROR"<<endl;
-      }
-    }
-    weight *= PUWeight;
-  }
-
-  double GetWeight(string type=""){
-      if (type=="" || type=="central"){
-	if(PUWeight == 1.){
-	  PUWeight= PUdata.at( relevantNumPU )/PUmc.at( relevantNumPU );
-	  return PUWeight;
-	}
-      }else if (type=="up"){
-	if(PUWeight_up==1.){
-	  PUWeight_up= PUdata_up.at( relevantNumPU )/PUmc.at( relevantNumPU );
-	  return PUWeight_up;
-	}
-      }else if (type=="down"){
-	if(PUWeight_down==1.){
-	  PUWeight_down= PUdata_down.at( relevantNumPU )/PUmc.at( relevantNumPU );
-	}
-	  return PUWeight_down;
-      }else{
-	cout<<"wrong pileup weight chice 2.ERROR"<<endl;
-      }    
-  }
-  
+  void RescaleWeight(float, double& weight, string type);
+  double GetWeight(string type);
 };
 
 
